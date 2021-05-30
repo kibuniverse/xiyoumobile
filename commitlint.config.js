d3306e4c8ad94672eff80f4d3e1eda95e8898e80
yankaizhi <yankaizhi@bytedance.com>
2021-05-30 18:20:55 +0800

    feat: 🎸 perfect wiki api
    完善wiki接口

--- commitlint.config.js
+++ commitlint.config.js
@@ -111,26 +111,6 @@ module.exports = {
 			body: {
 				description: 'Provide a longer description of the change',
 			},
-			isBreaking: {
-				description: 'Are there any breaking changes?',
-			},
-			breakingBody: {
-				description:
-					'A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself',
-			},
-			breaking: {
-				description: 'Describe the breaking changes',
-			},
-			isIssueAffected: {
-				description: 'Does this change affect any open issues?',
-			},
-			issuesBody: {
-				description:
-					'If issues are closed, the commit requires a body. Please enter a longer description of the commit itself',
-			},
-			issues: {
-				description: 'Add issue references (e.g. "fix #123", "re #123".)',
-			},
 		},
 	},
 };
