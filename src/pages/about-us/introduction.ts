8ba92f617ea239c43efba5a4186d627cae54c5a1
lovebaihezi <x18739168862@gmail.com>
2021-11-27 23:37:05 +0800

    feat: 🎸 about us
    add about us,left web img not update
    
    BREAKING CHANGE: 🧨 none
    
    ✅ Closes: none

---
+++ src/pages/about-us/introduction.ts
@@ -0,0 +1,24 @@
+export const Introduction = {
+  Android: {
+    picture_url: 'https://pixabay.com/photos/bear-computer-night-lilac-dark-2382779/',
+    text: 'Discord was started to solve a big problem: how to communicate with friends around the world while playing games online. Since childhood, founders Jason Citron and Stan Vishnevskiy both shared a love of video games, cherishing the friendships and connections that formed while playing them. At the time, all the tools built for this job were slow, unreliable, and complex. Jason and Stan knew they could make a better service that encouraged talking, helped form memories, and recreated the feeling of togetherness all found through gaming.'
+  },
+  IOS: {
+    picture_url: 'https://pixabay.com/photos/bear-computer-night-lilac-dark-2382779/',
+    text: 'Discord was started to solve a big problem: how to communicate with friends around the world while playing games online. Since childhood, founders Jason Citron and Stan Vishnevskiy both shared a love of video games, cherishing the friendships and connections that formed while playing them. At the time, all the tools built for this job were slow, unreliable, and complex. Jason and Stan knew they could make a better service that encouraged talking, helped form memories, and recreated the feeling of togetherness all found through gaming.'
+  },
+  Web: {
+    picture_url: 'https://pixabay.com/photos/bear-computer-night-lilac-dark-2382779/',
+    text: 'Discord was started to solve a big problem: how to communicate with friends around the world while playing games online. Since childhood, founders Jason Citron and Stan Vishnevskiy both shared a love of video games, cherishing the friendships and connections that formed while playing them. At the time, all the tools built for this job were slow, unreliable, and complex. Jason and Stan knew they could make a better service that encouraged talking, helped form memories, and recreated the feeling of togetherness all found through gaming.'
+  },
+  Server: {
+    picture_url: 'https://pixabay.com/photos/bear-computer-night-lilac-dark-2382779/',
+    text: 'Discord was started to solve a big problem: how to communicate with friends around the world while playing games online. Since childhood, founders Jason Citron and Stan Vishnevskiy both shared a love of video games, cherishing the friendships and connections that formed while playing them. At the time, all the tools built for this job were slow, unreliable, and complex. Jason and Stan knew they could make a better service that encouraged talking, helped form memories, and recreated the feeling of togetherness all found through gaming.'
+  }
+}
+
+export const summary =
+  '西安邮电大学移动应用开发实验室，亦称西安邮电大学3G手机应用开发实验室，创建于2011年，隶属于西安邮电大学计算机学院，专注于目前主流的两大移动平台的应用开发和Web开发，并以服务在校学生、丰富在校学生移动应用为目标，自行设计和开发了多款手机客户端软件以及PC、移动Web服务。实验室针对不同平台和成员兴趣，相应地划分为4个开发小组，包括苹果iOS、谷歌Android、后台Server和前端。实验室以自主学习与共同交流并行的自发式学习方式为主，培养成员的学习兴趣及自主学习能力，努力为每位成员创造最佳的学习环境和学习氛围，为打造一批移动应用开发能手而不断努力！' +
+  '实验室位于西安邮电大学长安校区东区教学楼一层FZ155教室，西邮IT应用型人才实训中心，是“Google 大学合作部--Android 人才培养示范基地”。实验室内部设有众多移动应用开发设备，包括若干台iMac一体机、Mac Mini以及PC，除此之外，各小组还配备有相关的测试设备，包括iPhone、iPad、iPod Touch、HTC Android智能手机以及Windows Phone 智能手机可供成员测试相关应用程序。Android开发组还配有由公司提供的Android底层开发箱，可用于开发和测试Android底层程序。' +
+  '实验室已开发多款不同平台的手机应用，包括iOS平台流媒体播放器，Android平台酷步软件，Windows Phone平台通讯中心，以及面向全平台的校园软件iXiyou和校园图书借阅查询系统客户端，实现了西邮图书馆图书随时随地查询借阅。由于成员作品比较多，此处不再一一列举。' +
+  '实验室每年针对全校所有热爱移动应用开发的同学进行纳新，只要对于移动应用开发感兴趣的同学，欢迎来实验室对纳新事宜进行咨询！'
